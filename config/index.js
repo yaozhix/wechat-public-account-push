247e534615c34120558e87b7f056921f6498e7be
yaozhix <114216701+yaozhix@users.noreply.github.com>
2022-09-23 16:06:47 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ export const USER_CONFIG = {
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "ebb43cad2d3aae7af532fa2d8cb4daf0",
+  APP_SECRET: "1b71292ceb4e25a3fc69261104f9c614",
   
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
