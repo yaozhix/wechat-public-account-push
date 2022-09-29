45318ad8750ac8780e6623a1f3ec8b322bb6d968
yaozhix <114216701+yaozhix@users.noreply.github.com>
2022-09-29 18:20:59 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -62,45 +62,6 @@ export const USER_CONFIG = {
       // 想要发送的人的名字
       name: "爹爹来报",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "oZnQ564zfhiLbFla1EZcSwz1VxgY",
-      // 你想对他发送的模板消息的模板ID
-      useTemplateId: "AoeTgWWDGfeV3V1TVLceYN6fBGoi70YHj4g3aN_wc2c",
-      // 所在省份
-      province: "福建",
-      // 所在城市
-      city: "莆田",
-      // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '02-26',
-      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
-      horoscopeDateType: '今日',
-      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://pvp.qq.com",
-      // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09"},
-        {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
-        {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
-      ],
-      // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
-      customizedDateList: [
-        // 在一起的日子
-        {"keyword": "love_day", date: "2022-09-08"},
-        // 结婚纪念日
-        {"keyword": "marry_day", date: "2022-09-09"},
-        // 退伍日
-        {"keyword": "ex_day", date: "2022-09-10"},
-      ]
-    },
-
-
-    // 你可以不断按格式往下增加
-    // ...
-     // 想要发送的人的名字
-      name: "爹爹来报",
-      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oZnQ569coTyH21zhURKxuuSmshaI",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "AoeTgWWDGfeV3V1TVLceYN6fBGoi70YHj4g3aN_wc2c",
@@ -133,6 +94,11 @@ export const USER_CONFIG = {
         {"keyword": "ex_day", date: "2022-09-10"},
       ]
     },
+
+
+    // 你可以不断按格式往下增加
+    // ...
+     
 
   ],
   
