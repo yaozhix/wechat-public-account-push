0017fe13347e2612dec3a5c11c1fd0326bca48a4
yaozhix <114216701+yaozhix@users.noreply.github.com>
2022-09-29 18:18:57 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -60,11 +60,11 @@ export const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "早安",
+      name: "爹爹来报",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oZnQ564zfhiLbFla1EZcSwz1VxgY",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "6-gjS4zq4r4MJ5BOFbB-Er9pfnEL6uq-212mj2hYJ6A",
+      useTemplateId: "AoeTgWWDGfeV3V1TVLceYN6fBGoi70YHj4g3aN_wc2c",
       // 所在省份
       province: "福建",
       // 所在城市
@@ -98,6 +98,42 @@ export const USER_CONFIG = {
 
     // 你可以不断按格式往下增加
     // ...
+     // 想要发送的人的名字
+      name: "爹爹来报",
+      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: "oZnQ569coTyH21zhURKxuuSmshaI",
+      // 你想对他发送的模板消息的模板ID
+      useTemplateId: "AoeTgWWDGfeV3V1TVLceYN6fBGoi70YHj4g3aN_wc2c",
+      // 所在省份
+      province: "福建",
+      // 所在城市
+      city: "福州",
+      // 新历生日, 仅用作获取星座运势, 格式必须
+      horoscopeDate: '06-02',
+      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
+      horoscopeDateType: '今日',
+      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+      openUrl: "https://pvp.qq.com",
+      // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09"},
+        {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
+        {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+      ],
+      // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
+      customizedDateList: [
+        // 在一起的日子
+        {"keyword": "love_day", date: "2022-09-08"},
+        // 结婚纪念日
+        {"keyword": "marry_day", date: "2022-09-09"},
+        // 退伍日
+        {"keyword": "ex_day", date: "2022-09-10"},
+      ]
+    },
+
   ],
   
   /**
@@ -105,7 +141,7 @@ export const USER_CONFIG = {
    */
   
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "6-gjS4zq4r4MJ5BOFbB-Er9pfnEL6uq-212mj2hYJ6A",
+  CALLBACK_TEMPLATE_ID: "AoeTgWWDGfeV3V1TVLceYN6fBGoi70YHj4g3aN_wc2c",
   
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
